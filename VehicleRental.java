4a39bbd49771e1972b2afc1d749ea0085f1ab451
krishkumarwork3-beep <krishkumar.work3@gmail.com>
2026-08-31 00:02:55 +0530

    made changes

--- VehicleRental.java
+++ VehicleRental.java
@@ -1,59 +1,43 @@
 public class VehicleRental {
-
-    static class Vehicle {
+        static class Vehicle {
         protected String vehicleNumber;
         protected String brand;
         protected int rentPerDay;
-
         public Vehicle(String vehicleNumber, String brand, int rentPerDay) {
             this.vehicleNumber = vehicleNumber;
             this.brand = brand;
             this.rentPerDay = rentPerDay;
         }
-
         public double calculateRent(int days) {
             return 0.0;
         }
-
         public void displayDetails() {
             System.out.println("vehicleNumber: " + vehicleNumber);
             System.out.println("brand: " + brand);
             System.out.println("rentPerDay: " + rentPerDay);
         }
     }
-
-    static class Car extends Vehicle {
-        private static final double LUXURY_CHARGE = 0.2;
-
+        static class Car extends Vehicle {
+        private static final double luxuryCharge = 0.2;
         public Car(String vehicleNumber, String brand, int rentPerDay) {
             super(vehicleNumber, brand, rentPerDay);
         }
-
-        @Override
         public double calculateRent(int days) {
-            return (rentPerDay + rentPerDay * LUXURY_CHARGE) * days;
+            return (rentPerDay + rentPerDay * luxuryCharge) * days;
         }
-
-        @Override
         public void displayDetails() {
             super.displayDetails();
             System.out.println("rentPaid: " + calculateRent(5));
         }
     }
-
     static class Bike extends Vehicle {
-        private static final double DISCOUNT = 0.1;
-
+        private static final double discount = 0.1;
         public Bike(String vehicleNumber, String brand, int rentPerDay) {
             super(vehicleNumber, brand, rentPerDay);
         }
-
-        @Override
         public double calculateRent(int days) {
-            return (rentPerDay - rentPerDay * DISCOUNT) * days;
+            return (rentPerDay - rentPerDay * discount) * days;
         }
-
-        @Override
         public void displayDetails() {
             super.displayDetails();
             System.out.println("rentPaid: " + calculateRent(5));
@@ -61,18 +45,16 @@ public class VehicleRental {
     }
 
     static class Truck extends Vehicle {
-        private static final int FLAT_CHARGE = 500;
+        private static final int flatCharge = 500;
 
         public Truck(String vehicleNumber, String brand, int rentPerDay) {
             super(vehicleNumber, brand, rentPerDay);
         }
 
-        @Override
         public double calculateRent(int days) {
-            return rentPerDay * days + FLAT_CHARGE;
+            return rentPerDay * days + flatCharge;
         }
 
-        @Override
         public void displayDetails() {
             super.displayDetails();
             System.out.println("rentPaid: " + calculateRent(5));
@@ -87,7 +69,6 @@ public class VehicleRental {
 
         for (Vehicle v : vehicles) {
             v.displayDetails();
-            System.out.println("-----");
         }
     }
 }
